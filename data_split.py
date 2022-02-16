04f62acf008405aed8e7a01cb5d9e8d7ddd43f30
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-02-16 22:26:23 +0900

    add outpath for data_split

--- data_split.py
+++ data_split.py
@@ -12,14 +12,14 @@ def get_ext(basename, exts=["jpg", "png"]):
     return None
 
 
-def split_data(dataset_path, percentage_test=20):
+def split_data(dataset_path, out_path,percentage_test=20):
     # Populate the folders
     p = percentage_test / 100
 
-    os.makedirs("tiles/images/valid", exist_ok=True)
-    os.makedirs("tiles/labels/valid", exist_ok=True)
-    os.makedirs("tiles/images/train", exist_ok=True)
-    os.makedirs("tiles/labels/train", exist_ok=True)
+    os.makedirs(f"{out_path}/images/valid", exist_ok=True)
+    os.makedirs(f"{out_path}/labels/valid", exist_ok=True)
+    os.makedirs(f"{out_path}/images/train", exist_ok=True)
+    os.makedirs(f"{out_path}/labels/train", exist_ok=True)
     for pathAndFilename in glob.iglob(os.path.join(dataset_path, "*.txt")):
         title, ext = os.path.splitext(os.path.basename(pathAndFilename))
         img_ext = get_ext(f"{dataset_path}/{title}")
@@ -28,11 +28,11 @@ def split_data(dataset_path, percentage_test=20):
             continue
 
         if random.random() <= p:
-            os.system(f"cp {dataset_path}/{title}.{img_ext} tiles/images/valid")
-            os.system(f"cp {dataset_path}/{title}.txt tiles/labels/valid")
+            os.system(f"cp {dataset_path}/{title}.{img_ext} {out_path}/images/valid")
+            os.system(f"cp {dataset_path}/{title}.txt {out_path}/labels/valid")
         else:
-            os.system(f"cp {dataset_path}/{title}.{img_ext} tiles/images/train")
-            os.system(f"cp {dataset_path}/{title}.txt tiles/labels/train")
+            os.system(f"cp {dataset_path}/{title}.{img_ext} {out_path}/images/train")
+            os.system(f"cp {dataset_path}/{title}.txt {out_path}/labels/train")
 
 
 if __name__ == "__main__":
@@ -40,5 +40,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--datapath", required=True, help="dataset path")
+    parser.add_argument("--outpath", required=True, help="output path")
     args = parser.parse_args()
-    split_data(args.datapath)
+    split_data(args.datapath, args.outpath)
